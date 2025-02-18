ee4571f70b7a41730cedbeefc1339a78256cb8db
Shreyas <shreyasrai23@gmail.com>
2025-02-17 17:07:40 -0800

    removed local tesseract setup

--- run.sh
+++ run.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-# Set Tesseract environment variables
-export TESSDATA_PREFIX="/usr/local/share/tessdata"
-export TESSERACT_CMD="/usr/local/bin/tesseract"
-
 echo "Running Redaction..."
 python3 redact_text.py
 
